24488ffaa9e20156bfd6f1b293dbb81b0dc24320
navkk <navkkrnair@gmail.com>
2019-10-11 10:34:57 +0530

    Changed a little

---
+++ src/main/java/com/kubia/KubiaApplication.java
@@ -0,0 +1,34 @@
+package com.kubia;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class KubiaApplication
+{
+	private static final Logger logger = LoggerFactory.getLogger(KubiaApplication.class);
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(KubiaApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public Map<String, String> init() throws UnknownHostException
+	{
+		String hostname = InetAddress.getLocalHost().getHostName();
+		logger.info("Executing init() from host {}", hostname);
+		return Collections.singletonMap("hostname", hostname);
+	}
+
+}
