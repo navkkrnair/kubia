3c362cc91060524c07986873ec05f2e59249e2cf
navkk <navkkrnair@gmail.com>
2019-10-19 11:32:07 +0530

    Multiport code complete

--- src/main/java/com/kubia/KubiaApplication.java
+++ src/main/java/com/kubia/KubiaApplication.java
@@ -12,7 +12,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,14 +37,24 @@ public class KubiaApplication
 		return Collections.singletonMap("hostname", hostname);
 	}
 
-	@Bean
-	public ServletWebServerFactory servletContainer(@Value("${server.http.port}") int httpPort)
-	{
-		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-		connector.setPort(httpPort);
 
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-		tomcat.addAdditionalTomcatConnectors(connector);
-		return tomcat;
+	@Bean
+	public WebServerFactoryCustomizer containerCustomizer(@Value("${server.http.port}") int httpPort)
+	{
+		return new WebServerFactoryCustomizer()
+		{
+			@Override
+			public void customize(WebServerFactory container)
+			{
+				if (container instanceof TomcatServletWebServerFactory)
+				{
+					TomcatServletWebServerFactory containerFactory = (TomcatServletWebServerFactory) container;
+
+					Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+					connector.setPort(httpPort);
+					containerFactory.addAdditionalTomcatConnectors(connector);
+				}
+			}
+		};
 	}
 }
