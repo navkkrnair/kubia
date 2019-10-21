2e878cc13c2a09822b63bc42989766f32efb5f87
navkk <navkkrnair@gmail.com>
2019-10-21 12:55:37 +0530

    Changed code to include readiness probe

--- src/main/java/com/kubia/KubiaApplication.java
+++ src/main/java/com/kubia/KubiaApplication.java
@@ -1,5 +1,7 @@
 package com.kubia;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Collections;
@@ -9,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +23,29 @@ public class KubiaApplication
 
 	public static void main(String[] args)
 	{
-		SpringApplication.run(KubiaApplication.class, args);
+		SpringApplication app = new SpringApplication(KubiaApplication.class);
+		app.addListeners((e) ->
+		{
+			if (e instanceof ApplicationReadyEvent)
+			{
+				File file = new File(File.separatorChar + "var" + File.separatorChar + "ready");
+				logger.info("Creating file at: " + file);
+				if (!(file.exists()))
+				{
+					try
+					{
+						file.createNewFile();
+						logger.info("created file: " + file);
+					}
+					catch (IOException e1)
+					{
+						logger.info("Creating file failed");
+					}
+
+				}
+			}
+		});
+		app.run(args);
 	}
 
 	@GetMapping("/")
