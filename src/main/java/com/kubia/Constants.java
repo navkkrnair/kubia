75a348070c3e2e9f819cd8a50281fb58cb64156e
navkk <navkkrnair@gmail.com>
2019-10-30 11:51:09 +0530

    Added jar again

--- src/main/java/com/kubia/Constants.java
+++ src/main/java/com/kubia/Constants.java
@@ -5,12 +5,12 @@ import java.io.File;
 public class Constants
 {
 	//To test in windows, use "D:" + File.separatorChar + "var" + File.separatorChar + "data" + File.separatorChar	+ "kubia.txt"; 
-	public static final String DATA_FILE = "D:" + File.separatorChar + "var" + File.separatorChar + "data" + File.separatorChar
-			+ "kubia.txt";
+	/*public static final String DATA_FILE = "D:" + File.separatorChar + "var" + File.separatorChar + "data" + File.separatorChar
+			+ "kubia.txt";*/
 
 	// To use in Docker/ Kubernetes 
-	/*public static final String DATA_FILE = File.separatorChar + "var" + File.separatorChar + "data" + File.separatorChar
-			+ "kubia.txt";*/
+	public static final String DATA_FILE = File.separatorChar + "var" + File.separatorChar + "data" + File.separatorChar
+			+ "kubia.txt";
 
 	public static final String SERVICE_NAME = "kubia-headless-service.default.svc.cluster.local";
 
